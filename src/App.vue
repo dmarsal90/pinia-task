96a1f7ed77b71111b5e90c2517259e8ce6a319bf
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-23 15:16:27 -0500

    Update Vue component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
-
 <template>
+  <main>
 
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo">
+    </header>
+
+  </main>
 </template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
