178e0db412436128ed492e6b1136f0bc9d04d4f7
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-23 22:16:01 -0500

    Update TaskDetails

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
-
+  <div class="task">
+    <h3>{{ task.title }}</h3>
+    <div class="icons">
+      <i class="material-icons">delete</i>
+      <i class="material-icons">favorite</i>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  props: ["task"],
+};
 </script>
