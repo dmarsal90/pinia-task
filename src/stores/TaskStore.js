b4ccb42d644feea39e5065fa701ed324f1fac9f0
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-23 21:52:20 -0500

    Update TaskStore

---
+++ src/stores/TaskStore.js
@@ -0,0 +1,11 @@
+import { defineStore } from "pinia";
+
+export const useTaskStore = defineStore("taskStore", {
+  state: () => ({
+    tasks: [
+      { id: 1, title: "tarea 1", isFav: false },
+      { id: 1, title: "tarea 2", isFav: false },
+        ],
+      name: 'Pinia',
+  }),
+});
