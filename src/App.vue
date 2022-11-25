f38c62df7353364391e51e16a36143b321109ad3
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-24 23:42:19 -0500

    Add filter

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@
       <h1>Pinia Tasks</h1>
     </header>
     <!-- tasks list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
       <p>All tasks</p>
       <div v-for="task in taskStore.tasks">
         <TaskDetails :task="task" />
       </div>
     </div>
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'favs'">
       <p>Fav tasks</p>
       <div v-for="task in taskStore.favs">
         <TaskDetails :task="task" />
@@ -24,12 +24,15 @@
 <script>
 import { useTaskStore } from "./stores/TaskStore.js";
 import TaskDetails from "./components/TaskDetails.vue";
+import { ref } from "vue";
 
 export default {
   components: { TaskDetails },
   //composition api
   setup() {
     const taskStore = useTaskStore();
+
+    const filter = ref('all');
 
     return { taskStore };
   },
