76df126a1f0cf8a03c1b5dd7f9ae36c4c9fe20e9
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-12-07 17:41:59 -0500

    Update useTaskStore

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -1,5 +1,10 @@
 import { defineStore } from "pinia";
 
 export const useTaskStore = defineStore('taskStore', {
-
+  state: () => ({
+    tasks: [
+        {id:1, title:'task 1', isFav: true},
+        {id:2, title:'task 2', isFav: false}
+    ]
+  })
 })
