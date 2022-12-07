2254736e5a15c6c38a856c65a0d239be3655af68
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-12-07 18:51:27 -0500

    Add filter for tasks to App

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,20 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'fav'">Favorite tasks</button>
+    </nav>
+
     <!-- task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
       <p>All tasks</p>
       <div v-for="task in taskStore.tasks">
         <TaskDetails :task="task"/>
       </div>
     </div>
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'fav'">
       <p>Fav tasks</p>
       <div v-for="task in taskStore.fav">
         <TaskDetails :task="task"/>
@@ -25,13 +31,17 @@
 <script>
 import { useTaskStore } from "./stores/TaskStore";
 import TaskDetails from "./components/TaskDetails.vue"
+import { ref } from "vue";
+
 
 export default {
   components: {TaskDetails},
   setup() {
     const taskStore = useTaskStore();
 
-    return { taskStore };
+    const filter = ref('all');
+
+    return { taskStore, filter };
   },
 };
 </script>
