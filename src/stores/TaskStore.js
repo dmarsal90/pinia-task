f552f370f0523daa78e7c1cfeb704dfeb7e8ff1d
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-12-07 19:13:58 -0500

    Add actions

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -20,4 +20,9 @@ export const useTaskStore = defineStore("taskStore", {
       return state.tasks.length;
     },
   },
+  actions: {
+    addTask(task) {
+      this.tasks.push(task);
+    },
+  },
 });
